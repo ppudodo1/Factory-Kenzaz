e59279a5e8e1bd1b1d1db51b5f98a557d013a61b
ppudodo <dkatavic03@gmail.com>
2024-07-17 09:13:33 +0200

    Added responzive header

--- src/components/generalComponents/header/Header.jsx
+++ src/components/generalComponents/header/Header.jsx
@@ -7,6 +7,8 @@ import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../../../firebase/firebase.js";
 import { useDispatch } from "react-redux";
 import { addUser, logOut } from "../../../features/loggedUser/userSlice.js";
+import hamburger from "../../../assets/logos/hamburger.png";
+import close from "../../../assets/logos/close.png";
 const buttonColors = {
   news: "#299EC3",
   business: "#EE6151",
@@ -18,6 +20,8 @@ const buttonColors = {
 
 const Header = () => {
   const [authUser, setAuthUser] = useState(null);
+  const [visibleNav, setVisibleNav] = useState(false);
+
   const dispatch = useDispatch();
   useEffect(() => {
     const listen = onAuthStateChanged(auth, (user) => {
@@ -32,6 +36,18 @@ const Header = () => {
       listen();
     };
   }, [dispatch]);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setVisibleNav(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   const userSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -47,18 +63,70 @@ const Header = () => {
   return (
     <header className={styles["header-outside-container"]}>
       <nav className={styles["header-inside-top"]}>
-        <div className={styles["header-inside-top-container"]}>
-          <div className={styles["logo-container"]}>
-            <div className={styles["inner-logo"]}>
-              <Link to={"/"}>
-                <img src={logo} alt="logo" className={styles["logo-image"]} />
+        <div
+          className={
+            visibleNav
+              ? styles["header-inside-top-container-mobile"]
+              : styles["header-inside-top-container"]
+          }
+        >
+          <div className={styles["logo-container-nav"]}>
+            <div className={styles["logo-container"]}>
+              <div className={styles["inner-logo"]}>
+                <Link to={"/"}>
+                  <img src={logo} alt="logo" className={styles["logo-image"]} />
+                </Link>
+              </div>
+              <Link to={"/"} className={styles["header-link"]}>
+                <h3 className={styles["header-title"]}>Kenaz</h3>
               </Link>
             </div>
-            <Link to={"/"} className={styles["header-link"]}>
-              <h3 className={styles["header-title"]}>Kenaz</h3>
-            </Link>
+            <button
+              onClick={() => setVisibleNav(!visibleNav)}
+              className={styles["mobile-nav"]}
+            >
+              {visibleNav ? (
+                <img src={close} alt="close" />
+              ) : (
+                <img src={hamburger} alt="hamburger" />
+              )}
+            </button>
           </div>
           <div className={styles["top-link-container"]}>
+            <a href="" className={styles["top-link"]}>
+              Media
+            </a>
+            <a href="" className={styles["top-link"]}>
+              Marketing
+            </a>
+            <a href="" className={styles["top-link"]}>
+              Contact
+            </a>
+            {authUser ? (
+              <>
+                <a href="/userProfile" className={styles["top-link"]}>
+                  {authUser.displayName}
+                </a>
+                <button onClick={userSignOut} className={styles["log-out"]}>
+                  Log out
+                </button>
+              </>
+            ) : (
+              <>
+                <a href="/login" className={styles["top-link"]}>
+                  Login
+                </a>
+                <a href="/signIn" className={styles["top-link"]}>
+                  Sign up
+                </a>
+              </>
+            )}
+            <div className={styles["search-icon"]}>
+              <img src={searchLogo} alt="searchLogo" />
+            </div>
+          </div>
+
+          <div className={visibleNav ? styles["visible"] : styles["invisible"]}>
             <a href="" className={styles["top-link"]}>
               Media
             </a>
@@ -94,7 +162,13 @@ const Header = () => {
         </div>
       </nav>
       <nav className={styles["header-inside-bottom"]}>
-        <div className={styles["header-inside-bottom-container"]}>
+        <div
+          className={
+            visibleNav
+              ? styles["header-inside-bottom-container-nav"]
+              : styles["header-inside-bottom-container"]
+          }
+        >
           {Object.entries(buttonColors).map(([key, color]) => (
             <Link
               key={key}
